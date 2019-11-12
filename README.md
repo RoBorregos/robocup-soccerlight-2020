9ad1ec400da25e2671ade09893143655d7684ffb
Keven Arroyo <dake.3601@gmail.com>
2019-11-12 12:47:10 -0600

    Creation of repo

---
+++ README.md
@@ -0,0 +1,1 @@
+RoboCup_SoccerLight_2020
---
+++ draft/draft.ino
@@ -0,0 +1,24 @@
+#include <Pixy2.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <math.h>
+#include <Wire.h>
+#include <HTInfraredSeeker.h>
+
+
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+  Serial.println("Test");
+}
